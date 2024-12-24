bc019cf444e5b2231755b7dc51520e7b2cc8e0fe
sunxihao <sunxh2016@lzu.edu.cn>
2024-12-24 23:13:57 +0800

    process

--- resort.py
+++ resort.py
@@ -20,7 +20,7 @@ def attention_scores(Q, K, V, mask=None):
     - 经过注意力加权的值张量
     """
     # 计算Q和K的点积
-    scores = torch.matmul(Q, K.transpose(-1, -2)) / math.sqrt(Q.size(-1))
+    scores = torch.matmul(Q, K.T) / math.sqrt(Q.size(-1))
 
     # 如果有掩码，应用掩码
     if mask is not None:
